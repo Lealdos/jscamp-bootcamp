74d68d2b496ff8277f6d2d25fb9bcb044e9ee16b
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-11-10 01:34:00 +0000

    feature: fetch data from data.json and rander it in the html

--- 02-ejercicio-javascript/fetch-data.js
+++ 02-ejercicio-javascript/fetch-data.js
@@ -1,1 +1,34 @@
 /* Aquí va la lógica para mostrar los resultados de búsqueda */
+
+
+
+const jobsListingSection = document.querySelector('.jobs-listings')
+
+
+
+const FetchData = fetch('./data.json')
+
+const article = FetchData.then(response => response.json()).then((jobs) => {
+    jobs.forEach(job => {
+
+        const jobListingCard = document.createElement('article')
+
+        jobListingCard.className = 'job-listing-card'
+        jobListingCard.dataset.modalidad = job.data.modalidad
+        jobListingCard.dataset.nivel = job.data.nivel
+        jobListingCard.dataset.technology = job.data.technology
+        jobListingCard.innerHTML = `<article class="job-listing-card">
+          <div>
+            <a href="trabajos-id-${job.id}.html"> 
+            <h3>${job.titulo}</h3>
+              <small>${job.empresa} | ${job.ubicacion}.</small>
+            </a>
+              <p>${job.descripcion}</p>
+              </div>
+              <button class="button-apply-job" id="boton-importante">Aplicar</button>
+        </article>`
+        jobsListingSection.appendChild(jobListingCard)
+    })
+})
+
+
