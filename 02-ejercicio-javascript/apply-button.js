de57393c5d2632300fa2ef927f232216536c3ab2
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-11-10 01:33:22 +0000

    feature: add functionality to change style when user click "aplicar" buttom

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -1,1 +1,11 @@
-/* Aquí va la lógica para dar funcionalidad al botón de "Aplicar" */
+const jobsListingSection = document.querySelector('.jobs-listings')
+
+jobsListingSection.addEventListener('click', function(event) {
+  const element = event.target
+
+  if (element.classList.contains('button-apply-job')) {
+    element.textContent = '¡Aplicado!'
+    element.classList.add('is-applied')
+    element.disabled = true
+  }
+})
