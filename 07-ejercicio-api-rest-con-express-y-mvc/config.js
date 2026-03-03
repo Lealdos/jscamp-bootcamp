352df9688d1f449d99afa4d8267d561323fb1854
Jose Leonardo Delgado Avila <j3leonardodelgado@gmail.com>
2026-03-02 20:45:19 -0500

    Add configuration file with CORS settings and default parameters

---
+++ 07-ejercicio-api-rest-con-express-y-mvc/config.js
@@ -0,0 +1,13 @@
+export const CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:1234',
+    'https://midu.dev',
+    'http://jscamp.dev',
+    'http://localhost:5173',
+];
+
+export const DEFAULTS = {
+    LIMIT_PAGINATION: 10,
+    LIMIT_OFFSET: 0,
+    PORT: 1234,
+};
