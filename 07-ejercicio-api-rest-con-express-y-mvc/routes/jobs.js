12fa3e71d026d10cdf34f95cfc9bcc5e8881507e
Jose Delgado <j3leonardodelgado@gmail.com>
2026-03-10 20:34:16 -0400

    Refactor jobs router to improve structure and remove unnecessary comments

--- 07-ejercicio-api-rest-con-express-y-mvc/routes/jobs.js
+++ 07-ejercicio-api-rest-con-express-y-mvc/routes/jobs.js
@@ -1,15 +1,13 @@
-import { Router } from 'express'
+import { Router } from 'express';
+import { JobsController } from '../controllers/jobs.js';
 
-export const jobsRouter = Router()
+const jobsRouter = Router();
 
-/* Aquí debe ir la lógica de tus rutas */
-/* Recuerda que en tus rutas debes usar los controladores */
-/* 
-Deberás implementar:
-- Obtener todos los jobs [GET]
-- Obtener un job por id [GET]
-- Crear un job [POST]
-- Actualizar un job por id [PUT]
-- Actualizar parcialmente un job por id [PATCH]
-- Eliminar un job por id [DELETE]
-*/
+jobsRouter.get('/', JobsController.getAll);
+jobsRouter.get('/:id', JobsController.getJobById);
+jobsRouter.post('/', JobsController.createJob);
+jobsRouter.put('/:id', JobsController.updateJobById);
+jobsRouter.patch('/:id', JobsController.partialUpdateJobById);
+jobsRouter.delete('/:id', JobsController.deleteJobById);
+
+export { jobsRouter };
