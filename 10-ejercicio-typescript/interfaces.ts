d5a95e60bba42893deaffd92e7108b9f76d5adf0
Jose Leonardo Delgado Avila <j3leonardodelgado@gmail.com>
2026-03-28 06:04:25 -0400

    Define function types in JobSearchService interface and implement JobApplication and DetailedJob interfaces

--- 10-ejercicio-typescript/interfaces.ts
+++ 10-ejercicio-typescript/interfaces.ts
@@ -1,26 +1,42 @@
 /* En este archivo deberás tipar las interfaces de los servicios de búsqueda y aplicación a empleo */
 
 import {
-  filterByExperience,
-  filterByMinSalary,
-  filterByTechnology,
-  searchJobs,
-} from './functions.ts'
+    filterByExperience,
+    filterByMinSalary,
+    filterByTechnology,
+    searchJobs,
+} from './functions.ts';
+import { Job } from './objects.ts';
+import { ApplicationStatus } from './types.ts';
 
 // Interface para servicios de búsqueda
 export interface JobSearchService {
-  /* Deberás definir los tipos de las funciones */
-}
-
-export const searchService: JobSearchService = {
-  searchJobs,
-  filterByExperience,
-  filterByMinSalary,
-  filterByTechnology,
+    searchJobs(jobs: Job[], text: string): Job[];
+    filterByExperience(jobs: Job[], level: string): Job[];
+    filterByTechnology(jobs: Job[], technology: string): Job[];
+    filterByMinSalary(jobs: Job[], minSalary: number): Job[];
 }
 
 // Interface para aplicación a empleo
-export interface JobApplication {}
+export interface JobApplication {
+    id: string;
+    jobId: string;
+    candidateId: string;
+    status: ApplicationStatus;
+    appliedDate: Date;
+    coverLetter?: string;
+}
 
 // Interface que extiende Job con propiedades adicionales
-export interface DetailedJob {}
+export interface DetailedJob extends Job {
+    benefits: string[];
+    requirements: string[];
+    applicationDeadline?: Date;
+}
+
+export const searchService: JobSearchService = {
+    searchJobs,
+    filterByExperience,
+    filterByMinSalary,
+    filterByTechnology,
+};
