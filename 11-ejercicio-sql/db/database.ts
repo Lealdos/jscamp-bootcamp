633397b81b30fba0664d21e07797d808df4eb266
Jose Delgado <j3leonardodelgado@gmail.com>
2026-08-08 11:10:11 -0400

    Initialize database connection with better-sqlite3 and set up database pragmas

--- 11-ejercicio-sql/db/database.ts
+++ 11-ejercicio-sql/db/database.ts
@@ -1,1 +1,12 @@
-/* Aquí irá tu código del primer ejercicio */
+import Database from 'better-sqlite3';
+import { fileURLToPath } from 'node:url';
+
+const DB_PATH = fileURLToPath(new URL('../jobs.db', import.meta.url));
+
+export const db = new Database(DB_PATH);
+
+db.pragma('journal_mode = WAL');
+
+db.pragma('foreign_keys = ON');
+
+export default db;
